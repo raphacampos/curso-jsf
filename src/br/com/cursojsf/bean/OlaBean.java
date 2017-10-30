47a43f000cc9693b749bb1b80d30ca6c5c285fce
raphael.campos <raphael.campos@RJOD212065.GOV>
2017-10-30 11:48:28 -0200

    primeiro commit

---
+++ src/br/com/cursojsf/bean/OlaBean.java
@@ -0,0 +1,23 @@
+package br.com.cursojsf.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean
+@SessionScoped
+public class OlaBean  implements Serializable{
+private static final long serialVersionUID = 1;
+private String nome;
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}	
+	
+
+	
+}
